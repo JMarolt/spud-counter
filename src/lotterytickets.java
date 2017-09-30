574bdea8f2591c2b55f23f2a5b46767918e69a85
GabrielZ1 <League@AdministorsiMac.attlocal.net>
2017-09-30 16:01:46 -0700

    Hi

---
+++ src/lotterytickets.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class lotterytickets {
+	public static void main(String[] args) {
+		for(int i = 0; i<5 ;i=i+9)	{
+			JOptionPane.showMessageDialog(null, "The lucky winners are...");
+	}
+
+}
+}
---
+++ src/potatosong.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class potatosong {
+public static void main(String[] args) {
+	for(int i=1;i<10;i=i+1) {
+		JOptionPane.showMessageDialog(null,i+ " potato,");
+		
+}
+
+	}	
+	
+
+
+	
+
+
+
+
+
+
+}
